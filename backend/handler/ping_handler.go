33398c84210cf6e78e249cd94bf0e991cf436cc8
MTXIK <nponomarev7545@gmail.com>
2025-02-03 21:30:33 +0300

    Add handlers prototypes

---
+++ backend/handler/ping_handler.go
@@ -0,0 +1,38 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/VK-Container-Pinger/backend/cache"
+	"github.com/VK-Container-Pinger/backend/models"
+	"github.com/VK-Container-Pinger/backend/repository"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+)
+
+type Handler struct {
+	Repo *repository.PingRepository
+	RedisClient *redis.Client
+}
+
+func NewHandler(repo *repository.PingRepository, redisClient *redis.Client) *Handler {
+	return &Handler{
+		Repo: repo,
+		RedisClient: redisClient,
+	}
+}
+
+func (h *Handler) GetPings(c *gin.Context){
+	
+}
+
+func (h *Handler) PostPing(c *gin.Context) {
+	
+}
+
+func (h *Handler) DeleteOldPings(c *gin.Context) {
+	
+}
